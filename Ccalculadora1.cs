a3197d361115f7bc606990e443a2020959a70eb5
Mariela_Gomez87 <73796143+MarielaGomez@users.noreply.github.com>
2021-05-10 22:10:05 -0300

    Add files via upload

---
+++ Ccalculadora1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalcu
+{
+    class Ccalculadora1
+    {
+        // esta clase es definida por nosotros
+
+        // declaramos los atributos(siempre privado, por ahora publico)
+        public double a;
+        public double b;
+        public double r;
+
+        // declaramos los metodos
+        public void Suma()
+        {
+            // se lleva acabo una accion
+            r = a + b;
+
+        }
+        public void Muestra()
+        {
+            Console.WriteLine("a={0}, b= {2)}, r={2}", a, b, r);
+        }
+    }
+    }
